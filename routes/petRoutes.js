61f8d2e3ee82ea2fd3a89126cabd4837c9f43d3d
frosipedro <pedro.frosi@sou.unijui.edu.br>
2024-05-08 13:36:15 -0300

    Reorganizei em pastas, atualizei rotas, comandos, arquivo env, vou comecar o desenvolvimento da API com Terminal (Inquirer, Chalk)

--- routes/petRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const petController = require('../controllers/petController')
-
-router.post('/:tutorId', petController.createPet)
-router.put('/:petId/tutor/:tutorId', petController.updatePet)
-router.delete('/:petId/tutor/:tutorId', petController.deletePet)
-
-module.exports = router
